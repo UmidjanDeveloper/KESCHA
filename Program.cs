0cacf6392c3cab3146d3c1a4749cdaa1a954abea
UmidjanUbaydullayev@gmail.com <UmidjanUbaydullayev@gmail.com>
2023-03-24 22:40:39 +0100

    INFRA: Upload Projects

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int KeschaAge = 5;
+Console.WriteLine("What is your name?");
+
+string name = Console.ReadLine();
+string greeting = $"Hello,  {name}";
+Thread.Sleep(2000);
+Console.WriteLine(greeting);  
+Console.WriteLine("What's your age?");
+string AgeString = Console.ReadLine();
+int Age = int.Parse(AgeString);
+Thread.Sleep(2000);
+string greeting2 = $"That's Awesome {name}.Nice to meet you";
+Console.WriteLine(greeting2);
+Thread.Sleep(2000);
+Console.WriteLine("Do you want to compare your age between Kescha's age?");
+string Confirmation = Console.ReadLine();
+Thread.Sleep(2000);
+Console.WriteLine("Wait......");
+Thread.Sleep(2000);
+Console.WriteLine($"The result is: {Age - KeschaAge}");
+Console.WriteLine($"And you are older than Kescha {Age > KeschaAge}");
+Console.WriteLine($"And you are younger than Kescha {Age > KeschaAge}");
+Console.WriteLine($"And you are younger than Kescha {Age == KeschaAge}")
