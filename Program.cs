1b582c45024b5d1c8f580f9ec8d2aa66ee32b11f
UmidjanUbaydullayev@gmail.com <UmidjanUbaydullayev@gmail.com>
2023-05-07 17:24:53 +0200

    new refactor

--- Program.cs
+++ Program.cs
@@ -12,39 +12,40 @@ namespace KESCHA
             Security security = new Security();
             security.CheckPassword();
 
-            // Greet user and ask for their name
-            Console.Write("What is your name?  ");
-            string name = Console.ReadLine();
-            string greeting = $"Hello, {name}";
-            Thread.Sleep(2000);
-            Console.WriteLine(greeting);
+            // Greet user and ask for their name and age
+            string userName;
+            int userAge;
+            User.GetUserInputs(out userName, out userAge);
 
-            // Ask for user's age
-            Console.Write("What's your age?  ");
-            int age = int.Parse(Console.ReadLine());
-            Thread.Sleep(2000);
-            string greeting2 = $"That's awesome, {name}. Nice to meet you.";
-            Console.WriteLine(greeting2);
-            Thread.Sleep(2000);
-
-            // Compare user's age with Kescha's age
+            // Create instance of Kescha and compare age with user's age
+            Animal kescha = new Animal(animalAge: 3, animalName: "Kescha");
             Console.WriteLine("Do you want to compare your age with Kescha's age?");
-            string confirmation = Console.ReadLine().ToLower();
-            Thread.Sleep(2000);
+            
+            // Validate user confirmation input
+            string confirmation;
+            do
+            {
+                confirmation = Console.ReadLine().ToLower();
+                if (confirmation != "yes" && confirmation != "no")
+                {
+                    Console.WriteLine("Wrong confirmation entered! Please enter 'yes' or 'no'.");
+                }
+            } while (confirmation != "yes" && confirmation != "no");
 
             if (confirmation == "yes")
             {
                 Console.WriteLine("Wait......");
                 Thread.Sleep(2000);
-                int keschaAge = 5;
-                int ageDifference = age - keschaAge;
-                Console.WriteLine($"The result is: {ageDifference}");
 
-                if (age > keschaAge)
+                // Calculate and display age difference between user and Kescha
+                kescha.AgeDifference = userAge - kescha.animalAge;
+                Console.WriteLine($"The age difference between you and Kescha is: {kescha.AgeDifference} years.");
+
+                if (userAge > kescha.animalAge)
                 {
                     Console.WriteLine("You are older than Kescha.");
                 }
-                else if (age == keschaAge)
+                else if (userAge == kescha.animalAge)
                 {
                     Console.WriteLine("You are the same age as Kescha.");
                 }
@@ -52,29 +53,26 @@ namespace KESCHA
                 {
                     Console.WriteLine("You are younger than Kescha.");
                 }
+
                 Thread.Sleep(2000);
-    
+
                 // Display Kescha's friends' details
                 Console.WriteLine("Now let me tell you about my friends.");
                 Thread.Sleep(2000);
 
-                string[] friendsName = {"Luna", "Iris", "Aurora"};
-                int[] friendsAge = {4, 6, 5};
-                string[] friendsAddress = {"Sydney", "Arizona", "Bangkok"};
+                string[] friendsName = { "Luna", "Iris", "Aurora", userName };
+                int[] friendsAge = { 4, 6, 5, userAge };
+                string[] friendsAddress = { "Sydney", "Arizona", "Bangkok", "Actually you didn't tell yet" };
 
                 for (int i = 0; i < friendsName.Length; i++)
                 {
-                    Console.WriteLine($"One of my friends is {friendsName[i]}. He is from {friendsAddress[i]} and he is {friendsAge[i]} years old");
+                    Console.WriteLine($"One of my friends is {friendsName[i]}. He is from {friendsAddress[i]} and he is {friendsAge[i]} years old.");
                 }
             }
             else if (confirmation == "no")
             {
-                Console.WriteLine("Okay, no problem");
-            }
-            else
-            {
-                Console.WriteLine("Wrong confirmation entered!");
+                Console.WriteLine("Okay, no problem.");
             }
         }
     }
-}
+} 
